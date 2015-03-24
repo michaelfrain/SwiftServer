2cb8e31e8f04e0b9c733952852dd96f55697ddef
Michael Frain <michael.frain@hudl.com>
2015-03-24 02:00:56 -0500

    first commit

---
+++ VideoConcat/StreamPlayController.swift
@@ -0,0 +1,13 @@
+//
+//  StreamPlayController.swift
+//  VideoConcat
+//
+//  Created by Michael Frain on 3/22/15.
+//  Copyright (c) 2015 Michael Frain. All rights reserved.
+//
+
+import UIKit
+
+class StreamPlayController: AVPlayerViewController {
+   
+}
